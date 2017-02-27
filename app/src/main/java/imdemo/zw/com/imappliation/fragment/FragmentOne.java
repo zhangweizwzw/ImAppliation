046b49d1488a8fbbe3f163311949c7c4e1461fae
zhangweizwzw <zhangwei_7154@163.com>
2017-02-27 10:46:20 +0800

    会话列表完成

--- app/src/main/java/imdemo/zw/com/imappliation/fragment/FragmentOne.java
+++ app/src/main/java/imdemo/zw/com/imappliation/fragment/FragmentOne.java
@@ -1,34 +1,69 @@
 package imdemo.zw.com.imappliation.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.hyphenate.EMMessageListener;
 import com.hyphenate.chat.EMClient;
-import com.hyphenate.chat.EMMessage;
-import com.hyphenate.chat.adapter.message.EMAMessage;
+import com.hyphenate.chat.EMConversation;
+import com.hyphenate.easeui.ui.EaseConversationListFragment;
 
-import java.util.List;
-
+import imdemo.zw.com.imappliation.ChatActivity;
 import imdemo.zw.com.imappliation.R;
-import imdemo.zw.com.imappliation.util.MyToast;
+import imdemo.zw.com.imappliation.common.Constant;
 
 /**
  * Created by admin on 2017/2/16.
  */
 
-public class FragmentOne extends Fragment {
-    private View view;
+public class FragmentOne extends EaseConversationListFragment {
+    private TextView errorText;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_one, container, false);
+    protected void initView() {
+        super.initView();
+        View errorView = (LinearLayout) View.inflate(getActivity(),R.layout.em_chat_neterror_item, null);
+        errorItemContainer.addView(errorView);
+        errorText = (TextView) errorView.findViewById(R.id.tv_connect_errormsg);
+    }
 
-        return view;
+    @Override
+    protected void setUpView() {
+        super.setUpView();
+        // register context menu
+        registerForContextMenu(conversationListView);
+        conversationListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                EMConversation conversation = conversationListView.getItem(position);
+                String username = conversation.conversationId();
+                if (username.equals(EMClient.getInstance().getCurrentUser()))
+                    Toast.makeText(getActivity(), R.string.Cant_chat_with_yourself, Toast.LENGTH_SHORT).show();
+                else {
+                    // start chat acitivity
+                    Intent intent = new Intent(getActivity(), ChatActivity.class);
+                    if (conversation.isGroup()) {
+                        if (conversation.getType() == EMConversation.EMConversationType.ChatRoom) {
+                            // it's group chat
+                            intent.putExtra(Constant.EXTRA_CHAT_TYPE, Constant.CHATTYPE_CHATROOM);
+                        } else {
+                            intent.putExtra(Constant.EXTRA_CHAT_TYPE, Constant.CHATTYPE_GROUP);
+                        }
+
+                    }
+                    // it's single chat
+                    intent.putExtra(Constant.EXTRA_USER_ID, username);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
 }
